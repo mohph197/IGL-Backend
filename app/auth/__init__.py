328283c4d35652295a9c2d86730a9efe5aaaf65d
NassimAm <nassimam2003@gmail.com>
2022-12-22 10:01:40 +0100

    Adding auth init and utils

---
+++ app/auth/__init__.py
@@ -0,0 +1,13 @@
+import os
+import pathlib
+from google_auth_oauthlib.flow import Flow
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  #this is to set our environment to https because OAuth 2.0 only supports https environments
+
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")  #set the path to where the .json file you got Google console is
+
+flow = Flow.from_client_secrets_file(  #Flow is OAuth 2.0 a class that stores all the information on how we want to authorize our users
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],  #here we are specifing what do we get after the authorization
+    redirect_uri="http://127.0.0.1:5000/callback"  #and the redirect URI is the point where the user will end up after the authorization
+)
---
+++ app/auth/utils.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from flask import Blueprint,session, abort, redirect, request
+from google.oauth2 import id_token
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
+def auth(flow):
+    authorization_url, state = flow.authorization_url()  #asking the flow class for the authorization (login) url
+    session["state"] = state
+    return redirect(authorization_url)
+
+def callback(flow):
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  #state does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+    
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience= os.environ['GOOGLE_CLIENT_ID']
+    )
+
+    session["google_id"] = id_info.get("sub")  #defing the results to show on the page
+    session["name"] = id_info.get("name")
+    
+    return redirect("/protected_area")  #the final page where the authorized users will end up
+
+def logout():
+    session.clear()
+    return redirect("/")
