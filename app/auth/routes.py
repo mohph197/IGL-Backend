a2643c2a8f5c843f229bda947f758be0ef2b7208
Mohammed Tirichine <mohtiri718@gmail.com>
2022-12-22 10:01:33 +0100

    Defining Auth Routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,3 +1,16 @@
 from flask import Blueprint
+from app.auth.utils import auth, callback, logout
 
 bp = Blueprint("auth", __name__)
+
+@bp.route("/")
+def auth_route():
+    return auth()
+
+@bp.route("/callback")
+def callback_route():
+    return callback()
+
+@bp.route("/logout")
+def logout_route():
+    return logout()
