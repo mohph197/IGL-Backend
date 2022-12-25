9ece386eeb10897f31aff3d1c4d8a12cd765139f
IslemMedjahdi <ki_medjahdi@esi.dz>
2022-12-25 23:31:16 +0100

    fixing credentials

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -8,7 +8,7 @@ from app.auth import flow
 from app import db,login_is_required
 from app.auth.models import *
 import jwt
-
+import json
 
 def insertUserToDb(email,prenom,nom,role):
     try:
@@ -35,30 +35,36 @@ def insertUserToDb(email,prenom,nom,role):
 
 
 def new_auth():
-    credentials = request.args.get('credentials')
+    json_string = request.get_data().decode('utf-8')
+    json_object = json.loads(json_string)
+    if "credentials" in json_object:
+        credentials = json_object["credentials"]
+        request_session = requests.session()
+        cached_session = cachecontrol.CacheControl(request_session)
+        token_request = google.auth.transport.requests.Request(session=cached_session)
 
-    request_session = requests.session()
-    cached_session = cachecontrol.CacheControl(request_session)
-    token_request = google.auth.transport.requests.Request(session=cached_session)
+        try:
+            id_info = id_token.verify_oauth2_token(
+                id_token=credentials,
+                request=token_request,
+                audience= os.environ['GOOGLE_CLIENT_ID']
+            )
+            result = insertUserToDb(email=id_info['email'],prenom=id_info['given_name'],nom=id_info['family_name'],role='U')
 
-    try:
-        id_info = id_token.verify_oauth2_token(
-            id_token=credentials,
-            request=token_request,
-            audience= os.environ['GOOGLE_CLIENT_ID']
-        )
+            token = jwt.encode({"email":id_info['email']},os.environ['SECRET_KEY'],algorithm=os.environ['JWT_ALGORITHM'])
 
-        result = insertUserToDb(email=id_info['email'],prenom=id_info['given_name'],nom=id_info['family_name'],role='U')
-
-        token = jwt.encode({"email":id_info['email']},os.environ['SECRET_KEY'],algorithm=os.environ['JWT_ALGORITHM'])
-
+            return jsonify({
+                "token":token
+            }),200
+        except Exception as e:
+            return jsonify({
+                "error": e.args,
+                "message": "Error"
+            }), 500
+    else:
         return jsonify({
-            "token":token
-        }),200
-    except Exception as e:
-        return jsonify({
-            "error": e.args,
-            "message": "Error"
+                "error": "credentials miss",
+                "message": "Error"
         }), 500
 
 def auth():
@@ -72,7 +78,7 @@ def me():
         bearer_token = authorization_header.split(' ')[1]
         login_info = login_is_required(bearer_token)
         if login_info:
-            user = db.session.query(User).filter_by(email=login_info.email).first()
+            user = db.session.query(User).filter_by(email=login_info["email"]).first()
             if user:
                 return jsonify({
                     "email":user.email,
